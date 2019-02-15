c0777e5b063457c78e49244bcd8a296e93609686
Christian Dräger <christian.draeger1@gmail.com>
2019-02-15 14:53:14 +0100

    add some java docs

--- src/test/kotlin/it/skrape/select/Selects.kt
+++ src/test/kotlin/it/skrape/select/Selects.kt
@@ -4,7 +4,23 @@ import it.skrape.core.Result
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
+/**
+ * Will pick the first occurrence of an Element that
+ * is matching the CSS-Selector.
+ * @see <a href="https://www.w3schools.com/cssref/css_selectors.asp">Overview of CSS-Selectors for further information.</a>
+ * @param selector that represents an CSS-Selector
+ * @return Element
+ * @see Element
+ */
 fun Result.el(selector: String): Element = this.document().selectFirst(selector)
 
+/**
+ * Will pick all occurrences of an Elements that are matching the CSS-Selector
+ * and return it as Elements which is basically a List<Element>
+ * @see <a href="https://www.w3schools.com/cssref/css_selectors.asp">Overview of CSS-Selectors for further information.</a>
+ * @param selector that represents an CSS-Selector
+ * @return Elements
+ * @see Elements
+ */
 fun Result.`$`(selector: String): Elements = this.document().select(selector)
 
