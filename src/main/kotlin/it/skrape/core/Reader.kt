1506eb880446a215079f0ebbbf64760dc532c554
Christian Dräger <christian-draeger1@gmail.com>
2019-04-16 14:34:42 +0200

    clean code

--- src/main/kotlin/it/skrape/core/Reader.kt
+++ src/main/kotlin/it/skrape/core/Reader.kt
@@ -5,8 +5,8 @@ import java.io.File
 import java.nio.charset.Charset
 
 internal class Reader(
-        var file: File,
-        var charset: Charset = Charsets.UTF_8
+        val file: File,
+        val charset: Charset = Charsets.UTF_8
 ) {
 
     internal fun read(): Doc = Jsoup.parse(file, charset.name(), "http://skrape.it/")
