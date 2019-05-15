ed60adc751c4ee9167085fa0e390ab03397f8100
Christian Dräger <christian-draeger1@gmail.com>
2019-05-15 09:40:00 +0200

    parse supports client side rendered DOM trees

--- src/main/kotlin/it/skrape/core/Parser.kt
+++ src/main/kotlin/it/skrape/core/Parser.kt
@@ -1,8 +1,19 @@
 package it.skrape.core
 
+import it.skrape.core.fetcher.BrowserFetcher
 import org.jsoup.Jsoup
+import java.util.*
 
 class Parser(var html: String = "") {
 
+    private val dataUriMimeType = "data:text/html;charset=utf-8;"
+
     fun parse(): Doc = Jsoup.parse(html)
+
+    fun parseDom(): Doc = BrowserFetcher(Request(url = html.toUriScheme())).fetch().document
+
+    private fun String.toUriScheme(): String {
+        val base64encoded = Base64.getEncoder().encodeToString(toByteArray())
+        return "${dataUriMimeType}base64,$base64encoded"
+    }
 }
--- src/test/kotlin/it/skrape/core/ParserTest.kt
+++ src/test/kotlin/it/skrape/core/ParserTest.kt
@@ -8,20 +8,53 @@ internal class ParserTest {
 
     @Test
     internal fun `can parse HTML`() {
-        val htmlAsString = """
-            <html>
-                <head>
-                    <title>some title</title>
-                </head>
-                <body>
-                    <p>some text</p>
-                </body>
-            </html>
-        """.trimIndent()
+        // given
+        val htmlAsString = getMarkupFromFile("example.html")
 
+        // when
         val result = Parser(htmlAsString).parse()
 
-        assertThat(result.title()).isEqualTo("some title")
-        assertThat(result.selectFirst("p").text()).isEqualTo("some text")
+        // then
+        assertThat(result.title()).isEqualTo("i'm the title")
+        assertThat(result.selectFirst("p").text()).isEqualTo("i'm a paragraph")
     }
+
+    @Test
+    internal fun `can parse HTML using uri scheme`() {
+        // given
+        val htmlAsString = getMarkupFromFile("example.html")
+
+        val result = Parser(htmlAsString).parseDom()
+
+        // then
+        assertThat(result.title()).isEqualTo("i'm the title")
+        assertThat(result.selectFirst("p").text()).isEqualTo("i'm a paragraph")
+    }
+
+    @Test
+    internal fun `can parse JS rendered HTML using uri scheme`() {
+        // given
+        val htmlAsString = getMarkupFromFile("js.html")
+
+        val result = Parser(htmlAsString).parseDom()
+
+        println(result.body())
+
+        assertThat(result.title()).isEqualTo("i'm the title")
+        assertThat(result.selectFirst("p").text()).isEqualTo("i'm a paragraph")
+        assertThat(result.selectFirst(".dynamic").text()).isEqualTo("I have been dynamically added via Javascript")
+    }
+
+    @Test
+    internal fun `can parse ES6 rendered HTML using uri scheme`() {
+        // given
+        val htmlAsString = getMarkupFromFile("es6.html")
+
+        val result = Parser(htmlAsString).parseDom()
+
+        assertThat(result.title()).isEqualTo("i'm the title")
+        assertThat(result.selectFirst("p").text()).isEqualTo("dynamically added")
+    }
+
+    private fun getMarkupFromFile(file: String) = javaClass.getResource("/__files/$file").readText()
 }
