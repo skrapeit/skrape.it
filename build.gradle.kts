0d3498dfa5a8abe5199e3b0c418f4d47218bd275
Christian Dräger <christian.draeger1@gmail.com>
2019-10-17 19:42:05 +0200

    use gradle plugin to simplify maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,12 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 plugins {
     kotlin("jvm") version "1.3.50"
-    `maven-publish`
-    signing
     jacoco
     id("org.jetbrains.dokka") version "0.10.0"
     id("com.github.ben-manes.versions") version "0.26.0"
     id("com.adarshr.test-logger") version "2.0.0"
     id("io.gitlab.arturbosch.detekt") version "1.1.1"
+    id("com.vanniktech.maven.publish") version "0.8.0"
 }
 
 testlogger {
@@ -68,29 +67,8 @@ tasks {
         options.encoding = "UTF-8"
     }
 
-    val sourcesJar by registering(Jar::class) {
-        archiveClassifier.set("sources")
-        from(sourceSets.main.get().allSource)
-    }
-
-    dokka {
-        dependsOn(javadoc)
-        outputFormat = "javadoc"
-        outputDirectory = javadoc.get().destinationDir!!.absolutePath
-    }
-
-    val javadocJar by registering(Jar::class) {
-        dependsOn(dokka)
-        archiveClassifier.set("javadoc")
-        from(javadoc.get().destinationDir)
-    }
-
-    artifacts {
-        archives(javadocJar)
-        archives(sourcesJar)
-    }
-
     test {
+        dependsOn(detekt)
         useJUnitPlatform()
         testLogging {
             events("passed", "skipped", "failed")
@@ -98,74 +76,6 @@ tasks {
     }
 }
 
-group = "it.skrape"
-version = "0-SNAPSHOT"
-description = "skrape{it}"
-
-publishing {
-    publications {
-        register("mavenJava", MavenPublication::class) {
-            artifactId = "skrapeit-core"
-            from(components["java"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
-            pom {
-                name.set("skrape{it}")
-                description.set("""A Kotlin-based testing/scraping/parsing library providing the ability 
-                    |to analyze and extract data from HTML (server & client-side rendered). 
-                    |It places particular emphasis on ease of use and a high level of readability 
-                    |by providing an intuitive DSL. First and foremost it aims to be a testing lib, 
-                    |but it can also be used to scrape websites in a convenient fashion.""".trimMargin())
-                url.set("https://docs.skrape.it")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("christian-draeger")
-                        name.set("Christian Dräger")
-                        email.set("christian.draeger1@gmail.com")
-                        url.set("https://github.com/christian-draeger")
-                        timezone.set("Europe/Berlin")
-                        properties.put("twitter", "@JvmDefault")
-                    }
-                }
-                issueManagement {
-                    system.set("github")
-                    url.set("https://github.com/skrapeit/skrape.it/issues")
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/skrapeit/skrape.it.git")
-                    developerConnection.set("scm:git:ssh://github.com:skrapeit/skrape.it.git")
-                    url.set("https://github.com/skrapeit/skrape.it/tree/master")
-                }
-                packaging = "jar"
-            }
-        }
-    }
-    repositories {
-        maven {
-            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-
-            url = if (project.version.toString().contains("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-            credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
-            }
-        }
-
-    }
+mavenPublish {
+    useMavenPublish = true
 }
-
-signing {
-    // useGpgCmd()
-    publishing.publications["mavenJava"]
-}
-
-// apply(from = "publish.gradle.kts")
