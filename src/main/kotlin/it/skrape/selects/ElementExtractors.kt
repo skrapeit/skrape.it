f9e1d338035307299c46d3ed5b374bacea24f6eb
Christian Dräger <christian.draeger1@gmail.com>
2019-10-22 16:46:12 +0200

    refactor element extractors

--- src/main/kotlin/it/skrape/selects/ElementExtractors.kt
+++ src/main/kotlin/it/skrape/selects/ElementExtractors.kt
@@ -33,41 +33,41 @@ fun <T> Elements.elements(cssSelector: String, init: Elements.() -> T): T {
 }
 
 @SkrapeItDslMarker
-fun <T> Elements.findFirst(init: Element.() -> T) = get(0).init()
+fun <T> Elements.findFirst(init: Element.() -> T) = first().init()
 
 @SkrapeItDslMarker
 fun <T> DomSelector.findFirst(init: Element.() -> T) = doc.element(toCssSelector(), init)
 
 @SkrapeItDslMarker
-fun <T> Elements.findByIndex(index: Int, init: Element.() -> T) = get(index).init()
+fun <T> Elements.findByIndex(index: Int, init: Element.() -> T) = elementAt(index).init()
 
 @SkrapeItDslMarker
 fun <T> DomSelector.findByIndex(index: Int, init: Element.() -> T): T =
         doc.elements(toCssSelector()).findByIndex(index, init)
 
 @SkrapeItDslMarker
-fun <T> Elements.findSecond(init: Element.() -> T) = get(1).init()
+fun <T> Elements.findSecond(init: Element.() -> T) = elementAt(1).init()
 
 @SkrapeItDslMarker
 fun <T> DomSelector.findSecond(init: Element.() -> T): T =
         doc.elements(toCssSelector()).findSecond(init)
 
 @SkrapeItDslMarker
-fun <T> Elements.findThird(init: Element.() -> T) = get(2).init()
+fun <T> Elements.findThird(init: Element.() -> T) = elementAt(2).init()
 
 @SkrapeItDslMarker
 fun <T> DomSelector.findThird(init: Element.() -> T): T =
         doc.elements(toCssSelector()).findThird(init)
 
 @SkrapeItDslMarker
-fun <T> Elements.findLast(init: Element.() -> T) = get(size - 1).init()
+fun <T> Elements.findLast(init: Element.() -> T) = last().init()
 
 @SkrapeItDslMarker
 fun <T> DomSelector.findLast(init: Element.() -> T): T =
         doc.elements(toCssSelector()).findLast(init)
 
 @SkrapeItDslMarker
-fun <T> Elements.findSecondLast(init: Element.() -> T) = get(size - 2).init()
+fun <T> Elements.findSecondLast(init: Element.() -> T) = elementAt(size - 2).init()
 
 @SkrapeItDslMarker
 fun <T> DomSelector.findSecondLast(init: Element.() -> T): T =
