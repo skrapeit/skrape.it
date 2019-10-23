244bd796d353b3f5682019ae8e9acff2a67f17fa
Christian Dräger <christian.draeger1@gmail.com>
2019-10-23 21:35:37 +0200

    cleanup

--- src/main/kotlin/it/skrape/selects/ElementExtractors.kt
+++ src/main/kotlin/it/skrape/selects/ElementExtractors.kt
@@ -7,32 +7,6 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 @SkrapeItDslMarker
-fun <T> Element.element(cssSelector: String, init: Element.() -> T): T {
-    val element = selectFirst(cssSelector) ?: throw ElementNotFoundException(cssSelector)
-    return element.init()
-}
-
-@SkrapeItDslMarker
-fun <T> Element.elements(cssSelector: String, init: Elements.() -> T): T {
-    val elements = select(cssSelector)
-    if (elements.isEmpty()) throw ElementNotFoundException(cssSelector)
-    return elements.init()
-}
-
-@SkrapeItDslMarker
-fun <T> Elements.element(cssSselector: String, init: Element.() -> T): T {
-    val element = select(cssSselector).first() ?: throw ElementNotFoundException(cssSselector)
-    return element.init()
-}
-
-@SkrapeItDslMarker
-fun <T> Elements.elements(cssSelector: String, init: Elements.() -> T): T {
-    val elements = select(cssSelector)
-    if (elements.isEmpty()) throw ElementNotFoundException(cssSelector)
-    return elements.init()
-}
-
-@SkrapeItDslMarker
 fun <T> Elements.findFirst(init: Element.() -> T) = first().init()
 
 @SkrapeItDslMarker
