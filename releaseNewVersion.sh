a047c52d19e36ad17cb27045e8036feedd2513b9
Christian Dräger <christian.draeger1@gmail.com>
2020-01-19 14:38:32 +0100

    cleanup

--- releaseNewVersion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-prev_version=$(cat gradle.properties | grep "VERSION_NAME" |  sed -E 's/VERSION_NAME=//')
-
-#git commit --allow-empty -m "Bump version to ${INPUT_STRING}."
-#git tag -a -m "Tag version ${INPUT_STRING}." "v$INPUT_STRING"
-#git push origin --tags
