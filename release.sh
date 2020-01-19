a1f1638e1f2d95957a79bc7e833850163a2454f5
Christian Dräger <christian.draeger1@gmail.com>
2020-01-19 14:47:19 +0100

    release process: cleanup tag before set

--- release.sh
+++ release.sh
@@ -2,10 +2,14 @@
 
 possibleReleaseTypes="major feature bug alpha beta rc"
 
+
 if [[ ! ${possibleReleaseTypes[*]} =~ $1 ]]; then
     echo "valid argument: [ ${possibleReleaseTypes[*]} ]"
     exit 1
 else
+  git tag -d "$1"
+  git push --delete origin "$1"
+
   git commit --allow-empty -m "trigger new '$1' release"
   git tag "$1"
   git push --tags
