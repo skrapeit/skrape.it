63d23df8995c73de1f608a5d9b19bb0385787036
Christian Dräger <christian.draeger1@gmail.com>
2020-08-23 19:42:59 +0200

    kick function names that contains spaces to hopefully fix dex compiling error on android

--- src/test/kotlin/it/skrape/matchers/MatchersKtTest.kt
+++ src/test/kotlin/it/skrape/matchers/MatchersKtTest.kt
@@ -3,12 +3,8 @@ package it.skrape.matchers
 import io.mockk.every
 import io.mockk.mockk
 import it.skrape.selects.DocElement
-import org.jsoup.nodes.Element
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.parallel.Execution
-import org.junit.jupiter.api.parallel.ExecutionMode
-import org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT
 
 internal class MatchersKtTest {
 
@@ -19,20 +15,17 @@ internal class MatchersKtTest {
     @Test
     internal fun `toBe is working type safe for Int`() {
         anInt toBe 200
-        anInt `to be` 200
     }
 
     @Test
     internal fun `toBe is working type safe for String`() {
         aString toBe "test"
-        aString `to be` "test"
     }
 
     @Test
     internal fun `toBe can handle expected is null`() {
         Assertions.assertThrows(AssertionError::class.java) {
             "null" toBe null
-            "null" `to be` null
         }
     }
 
@@ -40,61 +33,52 @@ internal class MatchersKtTest {
     internal fun `toBe can handle actual is null`() {
         Assertions.assertThrows(AssertionError::class.java) {
             aNullableString toBe "foo"
-            aNullableString `to be` "foo"
         }
     }
 
     @Test
     internal fun `toBe is working type safe for null String`() {
         aNullableString toBe null
-        aNullableString `to be` null
     }
 
     @Test
     internal fun `toNotBe is working type safe for null String`() {
         aString toBeNot null
-        aString `to be not` null
     }
 
     @Test
     internal fun `toContain is working type safe for String`() {
         aString toContain "es"
-        aString `to contain` "es"
     }
 
     @Test
     internal fun `toContain is throwing exception on assertion failure`() {
         Assertions.assertThrows(AssertionError::class.java) {
             aString toContain "foo"
-            aString `to contain` "foo"
         }
     }
 
     @Test
     internal fun `toNotContain is working type safe for String`() {
         aString toNotContain "foo"
-        aString `to not contain` "foo"
     }
 
     @Test
     internal fun `toNotContain is throwing exception on assertion failure`() {
         Assertions.assertThrows(AssertionError::class.java) {
             aString toNotContain "es"
-            aString `to not contain` "es"
         }
     }
 
     @Test
     internal fun `toContain is working with lists`() {
         listOf("foo", "bar") toContain "bar"
-        listOf("foo", "bar") `to contain` "bar"
     }
 
     @Test
     internal fun `toContain on lists is throwing exception on assertion failure`() {
         Assertions.assertThrows(AssertionError::class.java) {
             listOf("foo", "bar") toContain "schnitzel"
-            listOf("foo", "bar") `to contain` "schnitzel"
         }
     }
 
