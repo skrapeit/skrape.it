8bf81207607e551f36e31d823005fe7c044a080d
ruffCode <alexi@ruffcode.com>
2021-03-25 18:04:03 -0400

    fix: #137 add missing org.jetbrains:annotations

--- integrationtests/build.gradle.kts
+++ integrationtests/build.gradle.kts
@@ -3,10 +3,12 @@ plugins {
 }
 
 dependencies {
+    testImplementation("org.jetbrains:annotations:19.0.0")
     testImplementation(project(path = ":test-utils", configuration = "default"))
     testImplementation(project(path = ":html-parser", configuration = "default"))
     testImplementation(project(path = ":assertions", configuration = "default"))
     testImplementation(project(path = ":fetcher:basis-fetcher", configuration = "default"))
     testImplementation(project(path = ":fetcher:http-fetcher", configuration = "default"))
     testImplementation(project(path = ":fetcher:browser-fetcher", configuration = "default"))
+    testImplementation(project(path = ":fetcher:ktor-fetcher", configuration = "default"))
 }
