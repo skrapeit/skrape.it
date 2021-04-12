9d39427efd75f9e5f076d95b37543700d748f1e3
Christian Dräger <christian.draeger1@gmail.com>
2021-04-12 22:24:41 +0200

    docs: add example that prove current maven central release is working

--- examples/use-pre-release-version/src/test/kotlin/BleedingEdgeReleaseTest.kt
+++ examples/use-pre-release-version/src/test/kotlin/BleedingEdgeReleaseTest.kt
@@ -2,19 +2,19 @@
 // TODO: need to fix jitpack release after module split
 
 import it.skrape.core.htmlDocument
-import it.skrape.fetcher.HttpFetcher
-import it.skrape.fetcher.extract
-import it.skrape.fetcher.skrape
+import it.skrape.fetcher.*
 import it.skrape.selects.html5.a
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 import strikt.assertions.isGreaterThanOrEqualTo
 
 
 class BleedingEdgeReleaseTest {
 
     @Test
-    internal fun `can use latest aggregator version from jitpack`() {
+    internal fun `can use latest aggregator version from maven central and scrape page without JS`() {
         val projectsGithubStars = skrape(HttpFetcher) {
             request {
                 url = "https://github.com/skrapeit/skrape.it"
@@ -25,6 +25,45 @@ class BleedingEdgeReleaseTest {
                         a {
                             withClass = "social-count"
                             findFirst { text }
+                        }
+                    }
+                }
+            }
+        }
+
+        expectThat(projectsGithubStars).isGreaterThanOrEqualTo("239")
+    }
+
+    @Test
+    internal fun `can use latest aggregator version from maven central and scrape page with JS`() {
+        val title = skrape(BrowserFetcher) {
+            request {
+                url = "https://docs.skrape.it"
+            }
+            extract {
+                htmlDocument {
+                    titleText
+                }
+            }
+        }
+
+        expectThat(title).isEqualTo("Introduction - skrape{it}")
+    }
+
+    @Test
+    internal fun `can use latest aggregator version from maven central and scrape page async`() {
+        val projectsGithubStars = runBlocking {
+            skrape(AsyncFetcher) {
+                request {
+                    url = "https://github.com/skrapeit/skrape.it"
+                }
+                extract {
+                    htmlDocument {
+                        ".pagehead-actions" {
+                            a {
+                                withClass = "social-count"
+                                findFirst { text }
+                            }
                         }
                     }
                 }
