6ee7d564e014eb92609d055d7b00db073a5e8a45
Christian Dräger <christian.draeger1@gmail.com>
2021-04-19 00:04:03 +0200

    fix: exampole not using snapshot for now

--- examples/use-pre-release-version/build.gradle.kts
+++ examples/use-pre-release-version/build.gradle.kts
@@ -3,9 +3,15 @@ plugins {
 }
 
 repositories {
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    mavenCentral()
+//    maven {
+//        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+//    }
 }
 
 dependencies {
-    implementation("it.skrape:skrapeit:0-SNAPSHOT")
+//    implementation("it.skrape:skrapeit:0-SNAPSHOT") {
+//        isChanging = true
+//    }
+    implementation("it.skrape:skrapeit:1.0.0")
 }
