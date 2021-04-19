3df47937cd3224e21c7440e100fc5b453b20697e
Christian Dräger <christian.draeger1@gmail.com>
2021-04-19 09:06:18 +0200

    fix: add missing transitive dependency

--- fetcher/base-fetcher/build.gradle.kts
+++ fetcher/base-fetcher/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 dependencies {
     implementation(projects.dsl)
     implementation(Deps.Kotlin.reflect) // needed for extractIt when creating instance
+    implementation(Deps.KotlinX.Coroutines.core)
     implementation(Deps.KotlinX.Coroutines.jdk8)
 
     testImplementation(project(path = ":test-utils", configuration = "default"))
