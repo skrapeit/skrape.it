38ea8515b2789f95ddb4b5b77c179269e1af4e84
Christian Dräger <christian.draeger1@gmail.com>
2021-10-21 00:10:48 +0200

    build: migrate to kotlin 1.5

--- buildSrc/buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/buildSrc/src/main/kotlin/Deps.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val kotlin = "1.4.32"
+    const val kotlin = "1.5.31"
     const val coroutines = "1.5.0"
     const val ktor = "1.5.4"
     const val serialization = "1.0.1"
