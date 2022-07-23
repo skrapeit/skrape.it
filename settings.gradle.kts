60060c58bcc46fae65dd9904ff34a28c212379fb
Adam <897017+aSemy@users.noreply.github.com>
2022-07-23 18:52:03 +0200

    update settings plugin to use shared repo config, and update Gradle plugins to match existing version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,22 +24,11 @@ project(":http-fetcher").projectDir = file("fetcher/http-fetcher")
 project(":browser-fetcher").projectDir = file("fetcher/browser-fetcher")
 project(":async-fetcher").projectDir = file("fetcher/async-fetcher")
 
-pluginManagement {
-    val kotlin_version: String by settings
-    repositories {
-        gradlePluginPortal()
-    }
-    plugins {
-        kotlin("jvm") version kotlin_version
-        id("org.jetbrains.dokka") version kotlin_version
-        id("com.adarshr.test-logger") version "3.1.0"
-        `java-library`
-        `maven-publish`
-        signing
-        id("org.jetbrains.kotlinx.kover") version "0.5.0-RC2"
-        id("com.github.ben-manes.versions") version "0.39.0"
-        id("se.patrikerdes.use-latest-versions") version "0.2.18"
-        id("io.gitlab.arturbosch.detekt") version "1.19.0"
-        id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    }
+apply(from = "./buildSrc/repositories.settings.gradle.kts")
+
+@Suppress("UnstableApiUsage") // Central declaration of repositories is an incubating feature
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
 }
+
+// Gradle Plugin versions are defined in ./buildSrc/build.gradle.kts
