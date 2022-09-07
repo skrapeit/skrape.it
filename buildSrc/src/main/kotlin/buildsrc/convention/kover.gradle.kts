ef935bdb794cd556bb84d16296849e2c135a93e3
Adam <897017+aSemy@users.noreply.github.com>
2022-09-07 21:22:05 +0200

    fix Kover coverage

--- buildSrc/src/main/kotlin/buildsrc/convention/kover.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/kover.gradle.kts
@@ -1,10 +1,12 @@
 package buildsrc.convention
 
+import kotlinx.kover.api.IntellijEngine
+
 plugins {
     id("org.jetbrains.kotlinx.kover")
 }
 
 kover {
     // https://search.maven.org/artifact/org.jetbrains.intellij.deps/intellij-coverage-agent
-    engine.set(kotlinx.kover.api.IntellijEngine("1.0.681"))
+    engine.set(IntellijEngine("1.0.681"))
 }
