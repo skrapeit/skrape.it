450dd60d0dd881e951b8d4c34682e3c2392add83
Maximilian Micko <mcdjuady@googlemail.com>
2022-10-02 10:46:13 +0200

    Opt into kotlinx.coroutines.ExperimentalCoroutinesApi on all test sourceSets

--- buildSrc/src/main/kotlin/buildsrc/convention/kotlin-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/kotlin-multiplatform.gradle.kts
@@ -24,3 +24,24 @@ configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
         }
     }
 }
+
+afterEvaluate {
+    configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+        sourceSets {
+            val commonTest by getting
+            //Find all test sourceSets (every one should depend on commonTest) to enable ExperimentalCoroutinesApi
+            val testSourceSets = matching {
+                if (it == commonTest) return@matching true
+                if (it.dependsOn.isEmpty()) return@matching false
+                val toCheck = it.dependsOn.toMutableList()
+                while (toCheck.isNotEmpty()) {
+                    val check = toCheck.removeAt(0)
+                    if (check == commonTest) return@matching true
+                    toCheck += check.dependsOn
+                }
+                return@matching false
+            }
+            testSourceSets.onEach { println("Test sourceSet $it") }.forEach { it.languageSettings.optIn("kotlinx.coroutines.ExperimentalCoroutinesApi") }
+        }
+    }
+}
