9cbd11a10eba339638475410ba2ffdb74a44dc54
Maximilian Micko <mcdjuady@googlemail.com>
2022-12-07 11:31:20 +0100

    Fixed up JS mocks to work with the IR backend

--- assertions/src/jsTest/kotlin/it/skrape/matchers/MatchersKtTestJS.kt
+++ assertions/src/jsTest/kotlin/it/skrape/matchers/MatchersKtTestJS.kt
@@ -2,38 +2,47 @@ package it.skrape.matchers
 
 import it.skrape.selects.DocElement
 
-interface Handler {
+external interface Handler {
     val get: (obj: dynamic, prop: String) -> dynamic
     val set: (obj: dynamic, prop: String) -> dynamic
 }
 
 external class Proxy(obj: dynamic, handler: Handler)
 
-actual fun MatchersKtTest.mockDocElementListOfSize(mockSize:Int): List<DocElement> = Proxy(Any(), object : Handler {
-    override val get: (obj: dynamic, prop: String) -> dynamic = ::getFun
-    override val set = { obj: dynamic, prop: String ->
-        console.log("$obj <- $prop")
-    }
-
-    fun getFun(obj: dynamic, prop: String): dynamic {
-        return when(prop) {
-            "size" -> mockSize
-            else -> obj[prop]
+actual fun MatchersKtTest.mockDocElementListOfSize(mockSize: Int): List<DocElement> {
+    val proxy = Proxy(emptyList<DocElement>(), object : Handler {
+        override val get: (obj: dynamic, prop: String) -> dynamic = ::getFun
+        override val set = { obj: dynamic, prop: String ->
+            console.log("$obj <- $prop")
         }
-    }
-}).unsafeCast<List<DocElement>>()
 
-actual fun MatchersKtTest.mockDocElement(mockIsPresent:Boolean, mockCssSelector:String): DocElement = Proxy(Any(), object : Handler {
-    override val get: (obj: dynamic, prop: String) -> dynamic = ::getFun
-    override val set = { obj: dynamic, prop: String ->
-        console.log("$obj <- $prop")
-    }
-
-    fun getFun(obj: dynamic, prop: String): dynamic {
-        return when(prop) {
-            "isPresent" -> mockIsPresent
-            "toCssSelector" -> mockCssSelector
-            else -> obj[prop]
+        fun getFun(obj: dynamic, prop: String): dynamic {
+            return if (prop.startsWith("get_size")) {
+                { mockSize }
+            } else {
+                obj[prop]
+            }
         }
-    }
-}).unsafeCast<DocElement>()
+    }).unsafeCast<List<DocElement>>()
+    return proxy
+}
+
+actual fun MatchersKtTest.mockDocElement(mockIsPresent: Boolean, mockCssSelector: String): DocElement =
+    Proxy(Any(), object : Handler {
+        override val get: (obj: dynamic, prop: String) -> dynamic = ::getFun
+        override val set = { obj: dynamic, prop: String ->
+            console.log("$obj <- $prop")
+        }
+
+        fun getFun(obj: dynamic, prop: String): dynamic {
+            return if (prop.startsWith("get_isPresent")) {
+                { mockIsPresent }
+            } else if (prop.startsWith("get_toCssSelector")) {
+                { mockCssSelector }
+            } else if (prop == "toString") {
+                { obj.toString() }
+            } else {
+                obj[prop]
+            }
+        }
+    }).unsafeCast<DocElement>()
